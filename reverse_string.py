bfa9f89582d3020cfbabbdbc6a220198d6c8e9b4
mwrobbins56 <mwrobbins56@gmail.com>
2026-03-26 23:34:36 -0400

    Delete reverse_string.py

--- reverse_string.py
+++
@@ -1,9 +0,0 @@
-def reverse_string(in_str):
-  # Accept a string and return it reversed.
-  in_str = input("What would you like to reverse? ")
-  return in_str[::-1]
-
-in_str = " "
-reversed_str = reverse_string(in_str)
-# Output: revered input
-print(reversed_str)
